d2daa4ce17c848c857748e7ed359e0767c375dbb
Eric Turner <EricTurnerNV@gmail.com>
2015-08-12 21:16:35 -0700

    Models created. Ready to migrate

--- social/social/settings.py
+++ social/social/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'django_extensions',
+    'debug_toolbar',
 )
 
 MIDDLEWARE_CLASSES = (
